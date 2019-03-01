158433d28d1842fe7076315498fb900215bdeeef
Silas Burger <ybsilas@gmail.com>
2019-03-01 09:26:30 -0800

    fixed equation constant

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,12 @@ fn main() {
 
     let f_temp: f32 = trimmed.parse().unwrap();
 
-    let c_temp: f32 = (f_temp - 32.00) * 0.555;
+    let c_temp: f32 = (f_temp - 32.00) * 5.0 / 9.0;
 
     println!("Here's your number {}", c_temp);
 }
 
 
-// (32°F − 32) × 5/9 = 0°C
 
-//   let mut input_text = String::new();
-//     io::stdin()
-//         .read_line(&mut input_text)
-//         .expect("failed to read from stdin");
 
-//     let trimmed = input_text.trim();
-//     match trimmed.parse::<u32>() {
-//         Ok(i) => println!("your integer input: {}", i),
-//         Err(..) => println!("this was not an integer: {}", trimmed),
-//     };
+
