f288ebd94aa2d5349acd931214430efe8948d74a
Silas Burger <ybsilas@gmail.com>
2019-03-21 23:16:46 -0700

    added enum and loop for more rapid input/output

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,51 @@
-use std::io;
+use std::{io, process};
 
-fn main() {
-
-    println!("Enter your number here: ");
-
-    let mut input_text = String::new();
-
-    io::stdin().read_line(&mut input_text)
-        .expect("Failed to read line");
-
-    let trimmed = input_text.trim();
-
-    let f_temp: f32 = trimmed.parse().unwrap();
-
-    let c_temp: f32 = (f_temp - 32.00) * 5.0 / 9.0;
-
-    println!("Here's your number {}", c_temp);
+enum Temp {
+    C(f64),
+    F(f64),
 }
 
+fn convert_temp(temp: &Temp) -> f64 {
+    match temp {
+        &Temp::F(degrees) => (degrees - 32.0) / 1.8,
+        &Temp::C(degrees) => (degrees * 1.8) + 32.0,
+    }
+}
 
+fn print_temp(temp: &Temp) {
+    match temp {
+        &Temp::F(degrees) => println!("{}F = {}C", degrees, convert_temp(temp)),
+        &Temp::C(degrees) => println!("{}C = {}F", degrees, convert_temp(temp)),
+    };
+}
 
+fn main() {
+    println!("Press q to quit at any time");
+    loop {
+        let mut input_text = String::new();
+        println!("Enter a number followed by a C or F (Format: 32F): ");
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("Failed to read line");
 
+        let trimmed = input_text.trim();
 
+        if trimmed == "q" {
+            process::exit(1);
+        };
+
+        let (temp, scale) = trimmed.split_at(trimmed.len() - 1);
+        let temp: f64 = match temp.parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let temp = match scale {
+            "C" => Temp::C(temp),
+            "F" => Temp::F(temp),
+            _ => continue,
+        };
+
+        print_temp(&temp);
+    }
+}
